37db3efd88aba0ab262323aca6aabe9437bc636c
Jacob Peat <jacob.peat01@gmail.com>
2023-01-05 13:27:01 +0000

    Fix safari style bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import "./css/style.css"
 import Navbar from './components/Navbar';
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import logo from "../images/airbnb-logo.png"
+import "./Navbar.css"
 export default function Navbar(){
     return (
         <nav>
-            <img src={logo} />
+            <img className="nav-logo" src={logo} />
         </nav>
     )
 }
